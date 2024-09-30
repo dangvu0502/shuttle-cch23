9eb9427370c29cde85671e979c6503e282c10292
dangvh <dangvh@bravebits.vn>
2024-09-30 14:23:07 +0700

    Finish -1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn server_error() -> impl IntoResponse {
+    StatusCode::INTERNAL_SERVER_ERROR.into_response()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .fallback(server_error);
 
     Ok(router.into())
 }
