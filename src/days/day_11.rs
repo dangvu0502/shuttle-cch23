2ad55bd4c55ae3dc115b240cc3bc50915e19f3c3
danggg <dangvu0502@gmail.com>
2024-10-05 15:08:41 +0700

    Finish day_11

--- src/days/day_11.rs
+++ src/days/day_11.rs
@@ -1,12 +1,25 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{extract::Multipart, routing::post, Json, Router};
+use image::GenericImageView;
+use tower_http::services::ServeDir;
 
 pub(super) fn route() -> Router {
-    Router::new().route("/", get(not_found))
+    Router::new()
+        .nest_service("/assets", ServeDir::new("assets"))
+        .route("/red_pixels", post(count_pixels))
 }
 
-async fn not_found() -> impl IntoResponse {
-    (
-        StatusCode::NOT_IMPLEMENTED,
-        "I can't found it on https://console.shuttle.rs/cch",
-    )
+async fn count_pixels(mut multipart: Multipart) -> Json<usize> {
+    let Some(field) = multipart.next_field().await.unwrap() else {
+        return axum::Json(0);
+    };
+
+    let img = image::load_from_memory(field.bytes().await.unwrap().as_ref()).unwrap();
+
+    img.pixels()
+        .filter(|x| {
+            let [r, g, b, _] = x.2 .0;
+            u16::from(r) > (u16::from(g) + u16::from(b))
+        })
+        .count()
+        .into()
 }
