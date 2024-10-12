5708094f8b52e17175892b6359d2475ecd7bdd3e
danggg <dangvu0502@gmail.com>
2024-10-12 15:01:40 +0700

    Finish day_19

--- src/days/day_19.rs
+++ src/days/day_19.rs
@@ -1,154 +1,128 @@
-use std::collections::HashMap;
-use std::sync::Arc;
+use axum::{
+    extract::{
+        ws::{WebSocket, WebSocketUpgrade},
+        Path, State,
+    },
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
+use futures_util::{SinkExt, StreamExt};
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc, Mutex,
+    },
+};
+use tokio::{spawn, sync::broadcast::Sender};
 
-use axum::extract::ws::Message;
-use axum::extract::{Path, State, WebSocketUpgrade};
-use axum::response::IntoResponse;
-use axum::routing::{get, post};
-use axum::Router;
-use futures_util::{sink::SinkExt, stream::StreamExt};
-use tokio::sync::broadcast::Sender;
-use tokio::sync::{broadcast, Mutex};
-
-#[derive(Default, Debug)]
-struct Day19 {
-    broadcaster: Mutex<HashMap<u16, Sender<Arc<Tweet>>>>,
-    tweet_views: Mutex<usize>,
-}
-
-type Day19State = State<Arc<Day19>>;
-
-pub(super) fn route() -> Router {
-    Router::new()
-        .route("/ws/ping", get(ping))
-        .route("/ws/room/:room_number/user/:username", get(rooms))
-        .route("/reset", post(reset))
-        .route("/views", get(views))
-        .with_state(Arc::new(Day19::default()))
-}
-
-#[tracing::instrument(ret)]
-async fn ping(ws: WebSocketUpgrade) -> impl IntoResponse {
-    ws.on_upgrade(move |mut socket| async move {
-        let mut started = false;
-        while let Some(Ok(message)) = socket.recv().await {
-            tracing::debug!("Received message: {:?}", message);
-            match message {
-                Message::Text(text) => {
-                    if started && text == "ping" {
-                        tracing::debug!("Sending pong back to client as game started");
-                        socket
-                            .send(Message::Text("pong".to_string()))
-                            .await
-                            .unwrap();
-                    }
-                    if text == "serve" {
-                        started = true;
-                    }
-                }
-                Message::Binary(bin) => {
-                    tracing::debug!("Received binary: {:?}", bin);
-                }
-                Message::Ping(_) => {
-                    tracing::debug!("Received ping");
-                }
-                Message::Pong(_) => {
-                    tracing::debug!("Received pong");
-                }
-                Message::Close(_) => break,
-            }
-        }
-    })
-}
-
-#[tracing::instrument(ret)]
-async fn views(State(chat): Day19State) -> String {
-    chat.tweet_views.lock().await.to_string()
-}
-
-#[tracing::instrument(ret)]
-async fn reset(State(chat): Day19State) {
-    *chat.tweet_views.lock().await = 0;
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-struct Tweet {
-    #[serde(default)]
+#[derive(Clone, serde::Serialize)]
+struct Message {
     user: String,
     message: String,
 }
 
-#[tracing::instrument(skip(chat))]
-async fn rooms(
-    State(chat): Day19State,
-    Path((room_number, username)): Path<(u16, String)>,
-    ws: WebSocketUpgrade,
-) -> impl IntoResponse {
-    ws.on_upgrade(move |socket| async move {
-        tracing::info!(
-            message = "Connection established",
-            room_number = room_number,
-            username = username
-        );
-        let tx = chat
-            .broadcaster
-            .lock()
-            .await
-            .entry(room_number)
-            .or_insert_with(|| broadcast::channel(1024).0)
-            .clone();
-        let mut rv = tx.subscribe();
-        let (mut sender, mut receiver) = socket.split();
-        let mut send_task = tokio::spawn(async move {
-            while let Ok(tweet) = rv.recv().await {
-                if let Err(e) = sender
-                    .send(Message::Text(serde_json::to_string(&tweet).unwrap()))
-                    .await
-                {
-                    tracing::error!("Error sending tweet: {e:?}");
-                }
-                *chat.tweet_views.lock().await += 1;
-            }
-        });
-        let current_user = username.clone();
-        let mut recv_task = tokio::spawn(async move {
-            while let Some(Ok(message)) = receiver.next().await {
-                tracing::debug!("Received message: {:?}", message);
-                match message {
-                    Message::Text(text) => match serde_json::from_str::<Tweet>(&text) {
-                        Ok(mut tweet) => {
-                            tweet.user = current_user.clone();
-                            let tweet = Arc::new(tweet);
-                            if tweet.message.len() <= 128 && tx.send(tweet.clone()).is_err() {
-                                tracing::warn!("tweet {tweet:?} could not be sent");
-                            }
-                        }
-                        Err(e) => {
-                            tracing::error!("Error parsing tweet: {:?}", e);
-                        }
-                    },
-                    Message::Binary(bin) => {
-                        tracing::debug!("Received binary: {:?}", bin);
-                    }
-                    Message::Ping(_) => {
-                        tracing::debug!("Received ping");
-                    }
-                    Message::Pong(_) => {
-                        tracing::debug!("Received pong");
-                    }
-                    Message::Close(_) => break,
-                }
-            }
-        });
+#[derive(Clone)]
+struct Day19State {
+    view_count: Arc<AtomicU64>,
+    sockets: Arc<Mutex<HashMap<u64, Sender<Message>>>>,
+}
 
-        tokio::select! {
-            _ = (&mut send_task) => recv_task.abort(),
-            _ = (&mut recv_task) => send_task.abort(),
+pub(crate) fn route() -> Router {
+    Router::new()
+        .route("/ws/ping", get(ping_handler))
+        .route("/reset", post(reset))
+        .route("/views", get(views))
+        .route("/ws/room/:id/user/:user", get(room_handler))
+        .with_state(Day19State {
+            view_count: Arc::new(AtomicU64::new(0)),
+            sockets: Arc::new(Mutex::new(HashMap::new())),
+        })
+}
+
+async fn ping_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(ping)
+}
+
+async fn ping(mut ws: WebSocket) {
+    let mut served = false;
+    while let Some(Ok(msg)) = ws.recv().await {
+        if let Ok(msg) = msg.to_text() {
+            match msg {
+                "serve" => served = true,
+                "ping" if served => {
+                    let _ = ws.send("pong".into()).await;
+                }
+                _ => {}
+            }
         }
-        tracing::info!(
-            message = "Connection Dropped",
-            room_number = room_number,
-            username = username,
-        );
-    })
+    }
+}
+
+async fn reset(State(state): State<Day19State>) {
+    state.view_count.store(0, Ordering::Relaxed);
+}
+
+async fn views(State(state): State<Day19State>) -> impl IntoResponse {
+    state.view_count.load(Ordering::Relaxed).to_string()
+}
+
+async fn room_handler(
+    ws: WebSocketUpgrade,
+    Path((id, user)): Path<(u64, String)>,
+    State(state): State<Day19State>,
+) -> impl IntoResponse {
+    ws.on_upgrade(move |s| room(s, id, user, state))
+}
+
+#[derive(serde::Deserialize)]
+struct WsMsg {
+    message: String,
+}
+
+async fn room(ws: WebSocket, id: u64, user: String, state: Day19State) {
+    let send = {
+        let Ok(mut map) = state.sockets.lock() else {
+            return;
+        };
+
+        if let Some(ch) = map.get(&id) {
+            ch.clone()
+        } else {
+            let ch = Sender::new(128);
+            map.insert(id, ch.clone());
+            ch
+        }
+    };
+
+    let (mut ws_send, mut ws_recv) = ws.split();
+
+    let mut recv = send.subscribe();
+    let recv_task = spawn(async move {
+        while let Ok(message) = recv.recv().await {
+            if let Ok(message) = serde_json::to_string(&message) {
+                if ws_send.send(message.into()).await.is_ok() {
+                    state.view_count.fetch_add(1, Ordering::Relaxed);
+                } else {
+                    return;
+                }
+            }
+        }
+    });
+
+    while let Some(Ok(msg)) = ws_recv.next().await {
+        if let Ok(msg) = msg.into_text() {
+            if let Ok(WsMsg { message }) = serde_json::from_str::<WsMsg>(&msg) {
+                if message.len() <= 128 {
+                    let _ = send.send(Message {
+                        user: user.clone(),
+                        message,
+                    });
+                }
+            }
+        }
+    }
+
+    recv_task.abort();
 }
